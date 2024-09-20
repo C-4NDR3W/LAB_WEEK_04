524f5d1a47664be457a1e0fe8150ce5194d21ac7
budijavier <alvnaltair@gmail.com>
2024-09-20 19:38:31 +0700

    final

--- app/src/main/java/com/example/lab_week_03/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_03/CafeAdapter.kt
@@ -1,5 +1,11 @@
 package com.example.lab_week_03
 
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
 val TABS_FIXED = listOf(
     R.string.starbucks_title,
     R.string.janjijiwa_title,
